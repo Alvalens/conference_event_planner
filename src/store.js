dc9dbe98d6b2d84462de9181c920a0ef9dfffcd9
Alvalen <alvalen.shafel04@gmail.com>
2025-04-15 16:39:19 +0700

    feat: enhance conference event planner with AV and meal selection features

--- src/store.js
+++ src/store.js
@@ -1,9 +1,14 @@
-// store.js
-import { configureStore } from '@reduxjs/toolkit';
-import venueReducer from './venueSlice';
+import { configureStore } from "@reduxjs/toolkit";
+import venueReducer from "./venueSlice";
+import avReducer from "./avSlice";
+import mealsReducer from "./mealsSlice";
 
 export default configureStore({
-  reducer: {
-    venue: venueReducer,
-  },
+	reducer: {
+		venue: venueReducer,
+		av: avReducer,
+		meals: mealsReducer,
+	},
 });
+// This code sets up a Redux store using the Redux Toolkit. It imports the necessary functions and reducers from different slices (venueSlice, avSlice, and mealsSlice) and combines them into a single store. The store can then be used in a React application to manage global state related to venues, audiovisual equipment, and meals for a conference event.
+// The store is configured with the `configureStore` function, which simplifies the setup process and automatically includes useful middleware for development. This allows for a more efficient and organized way to manage state in a React application.
